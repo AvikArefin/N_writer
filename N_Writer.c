67f99b0b0b86a9fc87d319b797c36b49e656a23f
Crimson_leaf <86384286+AvikArefin@users.noreply.github.com>
2022-12-18 21:31:02 +0600

    high frequency wordlist

--- N_Writer.c
+++ N_Writer.c
@@ -20,7 +20,7 @@ Licensed under GPL 2.0
 
 #define ERASE_LINE() printf("\33[2K")
 #define RETURN_CARRIAGE() printf("\r")
-#define CLEAR_SCREEN() printf("\033[2J")
+// #define CLEAR_SCREEN() printf("\033[2J")
 
 #define GOTOXY(x, y) printf("\033[%d;%dH", y, x)
 
@@ -29,31 +29,36 @@ Licensed under GPL 2.0
 #define MOVE_FORWARD(x) printf("\033[%dC", x)
 #define MOVE_BACKWARD(x) printf("\033[%dD", x)
 
-// (-1, 1) would mean 1 row down and 1 column up
-void reative_cursor_mover(int row, int column)
-{
-    if (row > 0)
-        MOVE_FORWARD(row);
-    else
-        MOVE_BACKWARD(column);
-    if (column > 0)
-        MOVE_FORWARD(column);
-    else
-        MOVE_BACKWARD(column);
-}
+// GLOBAL VARIABLES
+short int x_position = 3, y_position = 4;
+int text_holder_index = 0;
 
-int word_counter(char *input, int char_count)
-{
-    int word_count = 0;
-    for (int i = 0; i < char_count; i++)
-    {
-        if (input[i] == ' ')
-        {
-            word_count++;
-        }
-    }
-    return word_count + 1;
-}
+
+// (-1, 1) would mean 1 row down and 1 column up
+// void reative_cursor_mover(int row, int column)
+// {
+//     if (row > 0)
+//         MOVE_FORWARD(row);
+//     else
+//         MOVE_BACKWARD(column);
+//     if (column > 0)
+//         MOVE_FORWARD(column);
+//     else
+//         MOVE_BACKWARD(column);
+// }
+//
+// int word_counter(char *input, int char_count)
+// {
+//     int word_count = 0;
+//     for (int i = 0; i < char_count; i++)
+//     {
+//         if (input[i] == ' ')
+//         {
+//             word_count++;
+//         }
+//     }
+//     return word_count + 1;
+// }
 
 int char_counter(const char *input)
 {
@@ -65,18 +70,13 @@ int char_counter(const char *input)
     return char_count;
 }
 
-double wpm_counter(int word_count, int time_taken)
-{
-    return (double)(word_count * 60) / time_taken;
-}
-
-void loop_func(char *str, int no)
-{
-    for (int i = 0; i < no; i++)
-    {
-        printf("%s", str);
-    }
-}
+// void loop_func(char *str, int no)
+// {
+//     for (int i = 0; i < no; i++)
+//     {
+//         printf("%s", str);
+//     }
+// }
 
 const char border[] = "================================================================\n";
 const char welcome[] = "Welcome to the typing test. Where only the fast remains";
@@ -84,27 +84,240 @@ const char instruction[] = "Type the text above.";
 
 int random_num_chooser()
 {
-    int i = 15, k = 0;
+    int i = 200, k = 0;
     k = rand() % i;
     return k;
 }
 
 const char *arr[] = {
-    "Baron of Hell",
+    "Baron",
+    "address",
+    "adequate",
+    "adjust",
+    "adjustment",
+    "admire",
+    "admission",
+    "admit",
+    "adolescent",
+    "adopt",
+    "adult",
+    "advance",
+    "advanced",
+    "advantage",
+    "adventure",
+    "advertising",
+    "advice",
+    "advise",
+    "adviser",
+    "advocate",
+    "affair",
+    "affect",
+    "afford",
+    "afraid",
+    "African",
+    "after",
+    "afternoon",
+    "again",
+    "against",
+    "age",
+    "agency",
+    "agenda",
+    "agent",
+    "aggressive",
+    "ago",
+    "agree",
+    "agreement",
+    "ah",
+    "ahead",
+    "aid",
+    "aide",
+    "AIDS",
+    "aim",
+    "air",
+    "aircraft",
+    "airline",
+    "airport",
+    "album",
+    "alcohol",
+    "alive",
+    "all",
+    "alliance",
+    "allow",
+    "ally",
+    "almost",
+    "alone",
+    "along",
+    "already",
+    "also",
+    "alter",
+    "alternative",
+    "although",
+    "always",
+    "amazing",
+    "American",
+    "among",
+    "amount",
+    "analysis",
+    "analyst",
+    "analyze",
+    "ancient",
+    "and",
+    "anger",
+    "angle",
+    "angry",
+    "animal",
+    "anniversary",
+    "announce",
+    "annual",
+    "another",
+    "answer",
+    "anticipate",
+    "anxiety",
+    "any",
+    "anybody",
+    "anymore",
+    "anyone",
+    "anything",
+    "anyway",
+    "anywhere",
+    "apart",
+    "apartment",
+    "apparent",
+    "apparently",
+    "appeal",
+    "appear",
+    "appearance",
+    "apple",
+    "application",
+    "apply",
+    "appoint",
+    "appointment",
+    "appreciate",
+    "approach",
+    "appropriate",
+    "approval",
+    "approve",
+    "Arab",
+    "architect",
+    "area",
+    "argue",
+    "argument",
+    "arise",
+    "arm",
+    "armed",
+    "army",
+    "around",
+    "arrest",
+    "arrival",
+    "arrive",
+    "art",
+    "article",
+    "artist",
+    "artistic",
+    "as",
+    "Asian",
+    "aside",
+    "ask",
+    "asleep",
+    "aspect",
+    "assault",
+    "assert",
+    "assess",
+    "asset",
+    "assign",
+    "assure",
+    "at",
+    "athlete",
+    "athletic",
+    "atmosphere",
+    "attach",
+    "attack",
+    "attempt",
+    "attend",
+    "case",
+    "cash",
+    "cast",
+    "cat",
+    "catch",
+    "attention",
+    "attitude",
+    "attorney",
+    "attract",
+    "attractive",
+    "attribute",
+    "audience",
+    "author",
+    "authority",
+    "auto",
+    "available",
+    "average",
+    "avoid",
+    "award",
+    "aware",
+    "awareness",
+    "away",
+    "awful",
+    "baby",
+    "back",
+    "background",
+    "bad",
+    "badly",
+    "bag",
+    "bake",
+    "balance",
+    "ball",
+    "ban",
+    "band",
+    "bank",
+    "bar",
+    "barely",
+    "barrel",
+    "barrier",
+    "base",
+    "baseball",
+    "basic",
+    "basically",
+    "basis",
+    "basket",
+    "basketball",
+    "bathroom",
+    "battery",
+    "battle",
+    "be",
+    "beach",
+    "bean",
+    "bear",
+    "beat",
+    "beautiful",
+    "beauty",
+    "because",
+    "become",
+    "bed",
+    "bedroom",
+    "beer",
+    "before",
+    "begin",
+    "beginning",
+    "behavior",
+    "behind",
+    "being",
+    "belief",
+    "believe",
+    "bell",
     "Demon",
     "Hellknight",
-    "Cyberdemon",
+    "demon",
     "Mancubus",
     "Revenant",
     "Doomimp",
-    "Zombieman",
+    "Zombie",
     "Shotguy",
     "Beholder",
     "Moloch",
     "Satyr",
     "Afrit",
-    "Cybermastermind",
-    "Nightmarecacodemon",
+    "Cybermaster",
+    "Nightmare",
 };
 
 int string_length(const char *str)
@@ -117,12 +330,12 @@ int string_length(const char *str)
     return len;
 }
 
-struct Power_array
-{
-    int word_num;
-    int arr[20];
-};
-struct Power_array text_arr;
+// struct Power_array
+// {
+//     int word_num;
+//     int arr[20];
+// };
+// struct Power_array text_arr;
 
 int text_holder_no = 0; // ADDED
 char text_holder[100] = "";
@@ -131,7 +344,7 @@ void text_to_write()
 {
     printf("  ");
     int i = 0;
-    int word_num = 0;      // NOT NEDDED ATM
+    // int word_num = 0;      // NOT NEDDED ATM
     text_holder[0] = '\0'; // Emptying a string
     while (i < 60)
     {
@@ -144,10 +357,10 @@ void text_to_write()
 
         i += char_counter(arr[random_num]) + 1;
 
-        text_arr.arr[word_num] = random_num; // NOT NEEDED ATM
-        word_num++;
+        // text_arr.arr[word_num] = random_num; // NOT NEEDED ATM
+        // word_num++;
     }
-    text_arr.word_num = word_num; // NOT NEEDED ATM
+    // text_arr.word_num = word_num; // NOT NEEDED ATM
 
     text_holder_no = char_counter(text_holder); // ADDED
 }
@@ -180,7 +393,6 @@ void change_screen_text()
     MOVE_FORWARD(2);
 }
 
-short int x_position = 3, y_position = 4;
 void notifier(char *str)
 {
     GOTOXY(2, 10);
@@ -192,17 +404,16 @@ void notifier(char *str)
     GOTOXY(x_position, y_position);
 }
 
-int text_holder_index = 0;
 
 void main_loop(int line_no)
 {
     double total_time_taken = 0.0;
     int total_char_count = 0;
     int total_word_count = 0;
-
+    int total_incorrect_char_count = 0;
     while (line_no >= 0)
     {
-        char input[70];
+        // char input[70];
 
         clock_t t;
         t = clock();
@@ -215,7 +426,7 @@ void main_loop(int line_no)
 
         for (int i = 0; i < text_holder_no; i++)
         {
-            char x = c_getch();
+            char x = getch();
             if (x == '\b')
             {
                 if (x_position > 1)
@@ -240,7 +451,6 @@ void main_loop(int line_no)
             }
             else if (x >= 'a' && x <= 'z' || x >= 'A' && x <= 'Z' || x == ' ')
             {
-                notifier("Character Right");
                 printf("%c", x);
                 x_position++;
                 if (x != text_holder[i])
@@ -249,6 +459,15 @@ void main_loop(int line_no)
                     str[0] = text_holder[i];
                     str[1] = '\0';
                     notifier(str);
+                }
+                else // When it is the correct letter
+                {
+                    notifier("Character CORRECT");
+                    total_char_count++;
+                    if (x == ' ')
+                    {
+                        total_word_count++;
+                    }
                 }
             }
         }
@@ -265,19 +484,16 @@ void main_loop(int line_no)
         xprintd("TIME TAKEN:", total_time_taken);
 
         // CHARACTER & WORD COUNT:
-        int char_count = char_counter(input);
-        total_char_count += char_count;
         xprinti("CHARACTER COUNT:", total_char_count);
-        int word_count = word_counter(input, char_count);
-        total_word_count += word_count;
         xprinti("WORD COUNT:", total_word_count);
 
         // WORDS PER MINUTE
-        double wpm = wpm_counter(total_word_count, total_time_taken);
+        double wpm = (double)(total_word_count * 60.0) / total_time_taken;
         xprintd("WPM:", wpm);
 
         printf("%s", border);
-        //----------------------------------------------------------------------
+
+        // //----------------------------------------------------------------------
         // SCREEN AND BACK TO INPUT
         change_screen_text();
 
@@ -287,7 +503,7 @@ void main_loop(int line_no)
 
 int main()
 {
-    CLEAR_SCREEN();
+    // CLEAR_SCREEN(); // NOTE: Do not use if you are outside vs code
     GOTOXY(1, 1);
 
     printf("%s", border);
@@ -306,7 +522,6 @@ int main()
 
     main_loop(line_no);
     // PAUSE FOR A BIT
-    char c;
-    scanf("%c", &c);
+    char c = getch();
     return 0;
 }
--- tests/one.c
+++ tests/one.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
+void super_print(const char x[])
+{
+    printf("| %-50s |\n", x);
+    
+}
+
 int main()
 {
 
-    const char *arr[] = {
-        "Baron of Hell",
-        "Demon",
-        "Hellknight",
-        "Cyberdemon",
-        "Mancubus",
-        "Revenant",
-        "Doomimp",
-        "Zombieman",
-        "Shotguy",
-        "Beholder",
-        "Moloch",
-        "Satyr",
-        "Afrit",
-        "Cybermastermind",
-        "Nightmarecacodemon",
-    };
+    // const char *arr[] = {
+    //     "Baron of Hell",
+    //     "Demon",
+    //     "Hellknight",
+    //     "Cyberdemon",
+    //     "Mancubus",
+    //     "Revenant",
+    //     "Doomimp",
+    //     "Zombieman",
+    //     "Shotguy",
+    //     "Beholder",
+    //     "Moloch",
+    //     "Satyr",
+    //     "Afrit",
+    //     "Cybermastermind",
+    //     "Nightmarecacodemon",
+    // };
+    const char x[] = "Hello there darkness my friend";
+    // char x[100] = "";
 
-    char x[100] = "";
-    strcat(x, arr[0]);
-    strcat(x, arr[1]);
-    printf("%s", x);
+    printf("%s\n", x);
+    super_print(x);
+    // strcat(x, arr[0]);
+    // strcat(x, arr[1]);
+    // printf("%s", x);
     return 0;
 }
